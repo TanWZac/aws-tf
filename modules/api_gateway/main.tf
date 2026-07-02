00333bc9e55fb3a2cc4b2bddffa502380ca4a62f
Wen Zheng Tan <100010968+TanWZac@users.noreply.github.com>
2026-07-02 22:10:04 +1000

    Add API Gateway HTTP API resources

---
+++ modules/api_gateway/main.tf
@@ -0,0 +1,88 @@
+resource "aws_cloudwatch_log_group" "api" {
+  name              = "/${var.name_prefix}/api-gateway"
+  retention_in_days = var.access_log_retention_days
+}
+
+resource "aws_apigatewayv2_api" "this" {
+  name          = "${var.name_prefix}-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = var.allowed_origins
+    allow_methods = var.allowed_methods
+    allow_headers = var.allowed_headers
+    max_age       = 300
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-api"
+  }
+}
+
+resource "aws_apigatewayv2_integration" "backend" {
+  api_id             = aws_apigatewayv2_api.this.id
+  integration_type   = "HTTP_PROXY"
+  integration_method = "ANY"
+  integration_uri    = var.backend_url
+
+  payload_format_version = "1.0"
+  timeout_milliseconds   = 30000
+}
+
+resource "aws_apigatewayv2_route" "root" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "ANY /"
+  target    = "integrations/${aws_apigatewayv2_integration.backend.id}"
+}
+
+resource "aws_apigatewayv2_route" "proxy" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.backend.id}"
+}
+
+resource "aws_apigatewayv2_stage" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = var.stage_name
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api.arn
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      httpMethod              = "$context.httpMethod"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      protocol                = "$context.protocol"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
+
+  default_route_settings {
+    throttling_burst_limit = var.throttling_burst_limit
+    throttling_rate_limit  = var.throttling_rate_limit
+  }
+}
+
+resource "aws_apigatewayv2_domain_name" "this" {
+  count = var.custom_domain_name != null && var.certificate_arn != null ? 1 : 0
+
+  domain_name = var.custom_domain_name
+
+  domain_name_configuration {
+    certificate_arn = var.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "this" {
+  count = var.custom_domain_name != null && var.certificate_arn != null ? 1 : 0
+
+  api_id      = aws_apigatewayv2_api.this.id
+  domain_name = aws_apigatewayv2_domain_name.this[0].id
+  stage       = aws_apigatewayv2_stage.this.id
+}
